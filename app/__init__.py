07f50755252e6f438061fa75c722afb3b50dc5f3
krishang <patel.krishang02@gmail.com>
2024-02-29 01:17:26 -0500

    Working feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,30 @@
+from app.commands import CommandHandler
+from app.commands.add import AddCommand
+from app.commands.subtract import SubtractCommand
+from app.commands.multiply import MultiplyCommand
+from app.commands.divide import DivideCommand
+from app.commands.greet import GreetCommand
+
+
 class App:
-    @staticmethod
-    def start() -> None:
-        print("Hello World. Type 'exit' to exit.")
+
+    def __init__(self):
+        self.command_handler = CommandHandler()
+        self.command_handler.register_command("greet", GreetCommand())
+        self.command_handler.register_command("add", AddCommand())
+        self.command_handler.register_command("subtract", SubtractCommand())
+        self.command_handler.register_command("multiply", MultiplyCommand())
+        self.command_handler.register_command("divide", DivideCommand())
+
+
+    def start(self):
+        print("Type 'exit' to exit.")
         
         while True:
             user_input = input(">>> ")
             if user_input.lower() == "exit":
                 print("Exiting...")
                 break
-            else:
-                # Here, you could add additional commands and their handling
-                print("Unknown command. Type 'exit' to exit.")
+            self.command_handler.execute_command(user_input)
 
         
